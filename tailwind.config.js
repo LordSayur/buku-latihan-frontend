05e7f21a780cff0bf729e2fc03ffca060bde5109
Lord Sayur <umar_317@hotmail.com>
2021-06-12 16:45:11 +0800

    💄Add theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: colors.yellow,
+      secondary: '',
+    },
   },
   variants: {
     extend: {},
