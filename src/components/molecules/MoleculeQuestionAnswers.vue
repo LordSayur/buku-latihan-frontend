aa83d6ece5f08ece87caf69c5f848ed54ed37b82
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:28:46 +0800

    ✨Add molecule question answers component

---
+++ src/components/molecules/MoleculeQuestionAnswers.vue
@@ -0,0 +1,35 @@
+<template inherit-attrs="false">
+  <div class="flex justify-center items-center mb-5">
+    <div v-for="(answer, index) in props.answers" :key="answer" class="m-2">
+      <input
+        :id="index.toString()"
+        :ref="`radio_${index.toString()}`"
+        type="radio"
+        name="mcq"
+        :value="answer"
+        @keyup.enter="submit"
+      />
+      <label class="ml-2" :for="answer">{{ answer }}</label>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps, onMounted, ref } from 'vue'
+
+  const props = defineProps({
+    answers: Object,
+  })
+
+  const radio_0 = ref<HTMLElement | null>(null)
+
+  const submit = () => {
+    radio_0.value?.click()
+    radio_0.value?.focus()
+  }
+
+  onMounted(() => {
+    radio_0.value?.click()
+    radio_0.value?.focus()
+  })
+</script>
