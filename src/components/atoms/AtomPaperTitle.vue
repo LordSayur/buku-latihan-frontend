16248542b284cd609d8aa047e1370b7a17c94437
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:28:01 +0800

    ✨Add atom paper title component

---
+++ src/components/atoms/AtomPaperTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="m-5 flex flex-col items-center">
+    <h1>{{ title }}</h1>
+    <h2>{{ subTitle }}</h2>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  const props = defineProps({
+    title: String,
+    subTitle: String,
+  })
+</script>
