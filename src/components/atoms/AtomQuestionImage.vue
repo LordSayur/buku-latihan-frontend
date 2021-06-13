a952532be059ce47115c5298c6ef13c1cf5edee3
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:28:13 +0800

    ✨Add atom question image component

---
+++ src/components/atoms/AtomQuestionImage.vue
@@ -0,0 +1,10 @@
+<template inherit-attrs="false">
+  <div class="mb-5 flex justify-center h-80">
+    <img class="h-full" v-bind="attrs" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useContext } from 'vue'
+  const { attrs } = useContext()
+</script>
