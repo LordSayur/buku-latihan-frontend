a231c9d0bb58968efaee3048285100e2378aac4e
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:33:43 +0800

    ✨Home: Add list of subjects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1 class="text-xl">Home</h1>
-  <HelloWorld msg="Hello Vue 3 + Typescript + Vite"></HelloWorld>
+  <h1 class="text-xl">Subjects</h1>
+
+  <ul>
+    <li v-for="subject in subjects" :key="subject.id">
+      <router-link :to="{ name: 'Paper', params: { id: subject.id } }">
+        {{ subject.name }}
+      </router-link>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import { getData } from '@/composables/getData'
+
+  const { getAllSubjectsDetails } = getData()
+
+  const subjects = getAllSubjectsDetails()
 </script>
 
 <style scoped></style>
