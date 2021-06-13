341f45357f7acec81c1079275b2a79a0d1640bd0
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:34:12 +0800

    ♻App: Refactor file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="flex flex-col text-center">
-    <nav>
-      <ul class="flex justify-center">
-        <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
-        <li>|</li>
-        <li><router-link :to="{ name: 'About' }">About</router-link></li>
-      </ul>
-    </nav>
+    <OrganismHeader />
+    <OrganismNavBar />
     <router-view></router-view>
   </div>
 </template>
