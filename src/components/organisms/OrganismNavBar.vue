00e9702ad0fbaf283c96cadcf542145f20c93704
Lord Sayur <umar_317@hotmail.com>
2021-06-13 18:29:18 +0800

    ✨Add organism nav bar component

---
+++ src/components/organisms/OrganismNavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <ul class="flex justify-center">
+      <li class="p-2 m-2 border-b">
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+      </li>
+      <li class="p-2 m-2 border-b">
+        <router-link :to="{ name: 'About' }">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
